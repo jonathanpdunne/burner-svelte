305225d4492db9b694d95da39853f7d0a589bafe
Jonathan Dunne <jonathan@flexdapps.com>
2019-06-03 09:20:19 +1000

    svelte BP

---
+++ README.md
@@ -0,0 +1,10 @@
+# burner-svelte
+
+```bash
+npm install # or yarn!
+npm run dev
+```
+
+Open up [localhost:3000](http://localhost:3000) and start clicking around.
+
+## uses: https://github.com/sveltejs/sapper-template
---
+++ src/routes/index.svelte
@@ -0,0 +1,44 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>FLEXBUXX</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<section class="flex flex-column justify-between pa4 pt5">
+	<div class="wallet-status">
+	<div class="description">
+	The Ceremonious Flex Dapps© NYC Blockchain Week Debreif Voting Experience
+	</div>
+	<div class="f-subheadline">
+		<!-- =${state.CURRENCY_SYMBOL}${state.wallet.tokenBalance.toLocaleString() ||
+		0}= -->
+	</div>
+	</div>
+	<div class="actions flex flex-column tc">
+	<a href="/dapps">VOTE</a>
+	</div>
+</section>
